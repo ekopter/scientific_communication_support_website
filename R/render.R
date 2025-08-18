ceb542a66ca6cbec4036da92782eda01f34f0d10
ekopter <40898762+ekopter@users.noreply.github.com>
2025-08-18 12:48:33 +0200

    icon adjustments

--- R/render.R
+++ R/render.R
@@ -1,5 +1,3 @@
 setwd("vignettes")                   # enter vignettes sub-folder
 bookdown::render_book("index.Rmd")   # compile
 setwd("..")                          # return to base directory
-
-
